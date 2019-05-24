84f6d0c4597605cdd7b2288c7ef0b2e7984ed15e
wangkj <wangkj@linkcld.com>
2019-05-24 23:16:48 +0800

    添加feign-service

---
+++ feign-service/src/main/java/com/wotrd/feignservice/FeignServiceApplication.java
@@ -0,0 +1,14 @@
+package com.wotrd.feignservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class FeignServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeignServiceApplication.class, args);
+    }
+
+}
--- provider-service/src/main/java/com/wotrd/providerservice/ProviderServiceApplication.java
+++ provider-service/src/main/java/com/wotrd/providerservice/ProviderServiceApplication.java
@@ -21,6 +21,7 @@ public class ProviderServiceApplication {
     @RequestMapping(value = "/echo/{string}", method = RequestMethod.GET)
     public String echo(@PathVariable String string) {
         return "Hello Nacos Discovery " + string;
+
     }
 
 }
