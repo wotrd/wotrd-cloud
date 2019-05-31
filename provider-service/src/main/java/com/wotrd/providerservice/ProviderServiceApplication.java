4775b38ed893e0894e9cee9b0490882af44060cf
wangkj <wangkj@linkcld.com>
2019-05-31 13:19:26 +0800

    oauth2 数据库认证服务完成

--- provider-service/src/main/java/com/wotrd/providerservice/ProviderServiceApplication.java
+++ provider-service/src/main/java/com/wotrd/providerservice/ProviderServiceApplication.java
@@ -2,19 +2,11 @@ package com.wotrd.providerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-@EnableDiscoveryClient
-@RestController
 @SpringBootApplication
 public class ProviderServiceApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(ProviderServiceApplication.class, args);
     }
 
--- provider-service/src/test/java/com/wotrd/providerservice/ProviderService1ApplicationTests.java
+++ provider-service/src/test/java/com/wotrd/providerservice/ProviderService1ApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ProviderServiceApplicationTests {
+public class ProviderService1ApplicationTests {
 
     @Test
     public void contextLoads() {
