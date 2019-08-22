863cefaf1661c7acae70ddf76ade84e4fc9728bf
wangkj <wangkj@linkcld.com>
2019-08-22 09:32:50 +0800

    添加kafka的producer的同步，异步，事务发送消息

--- kafkatemplate/src/main/java/com/wotrd/kafkatemplate/controller/KafkaController.java
+++ kafkatemplate/src/main/java/com/wotrd/kafkatemplate/controller/KafkaController.java
@@ -1,21 +1,28 @@
 package com.wotrd.kafkatemplate.controller;
 
-import com.alibaba.fastjson.JSONObject;
-import com.wotrd.kafkatemplate.domain.Fool;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.PartitionInfo;
+import org.apache.kafka.common.protocol.types.Field;
+import org.springframework.kafka.core.KafkaOperations;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.FailureCallback;
 import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.SuccessCallback;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.concurrent.CompletableFuture;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 /**
  * 使用KafkaTemplate操作kafka
  * send需要加topic参数，sendDefault使用默认topic需要在应用配置文件中配置
+ * topic在发送时会自动进行创建
  */
 @Slf4j
 @RequestMapping("kafka")
@@ -26,23 +33,86 @@ public class KafkaController {
     private KafkaTemplate<String, String> kafkaTemplate;
 
     /**
-     * 发送消息
+     * 异步发送消息
      *
-     * @param msg
+     * @param topic
+     * @param partition
+     * @param key
+     * @param value
      * @return
      */
-    @RequestMapping("sendMsg")
-    public String sendMsg(@RequestParam String msg) {
+    @RequestMapping("asyncSendMsgWithTopic")
+    public String asyncSendMsgWithTopic(@RequestParam String topic, @RequestParam int partition, @RequestParam String key,
+                                        @RequestParam String value) {
+        final String[] result = new String[1];
+        ListenableFuture<SendResult<String, String>> future = kafkaTemplate.send(topic, partition, key, value);
+        future.addCallback((SendResult<String, String> sendResult) -> {
+            // todo success deal
+            result[0] = "success";
+        }, (Throwable ex) -> {
+            // todo error deal
+            result[0] = "failed";
+        });
+        return result[0];
+    }
 
-        Fool fool = Fool.builder().fool(msg).build();
-        log.info("message={}", JSONObject.toJSONString(fool));
-        //topic在发送时会自动进行创建
-        ListenableFuture<SendResult<String, String>> future = kafkaTemplate.send("kafka1", JSONObject.toJSONString(fool));
+    /**
+     * 同步发送消息,最大等待时间10秒钟
+     *
+     * @param topic
+     * @param partition
+     * @param key
+     * @param value
+     * @return
+     */
+    @RequestMapping("syncSendMsgWithTopic")
+    public String syncSendMsgWithTopic(@RequestParam String topic, @RequestParam int partition, @RequestParam String key,
+                                       @RequestParam String value) {
+        try {
+            SendResult<String, String> result = kafkaTemplate.send(topic, partition, key, value).get(10, TimeUnit.SECONDS);
+            return "success";
+        } catch (InterruptedException e) {
+            log.error(e.getMessage(), e);
+        } catch (ExecutionException e) {
+            log.error(e.getMessage(), e);
+        } catch (TimeoutException e) {
+            log.error(e.getMessage(), e);
+        }
+        return "failed";
+    }
 
-        CompletableFuture<SendResult<String, String>> future1 = future.completable();
-        boolean done = future1.isDone();
+    /**
+     * 实现事务
+     * 1Spring可以使用@Transactional实现
+     * 2KafkaTemplate实现
+     *
+     * @param topic
+     * @return
+     */
+    @RequestMapping("executeTransaction")
+    public boolean executeTransaction(@RequestParam String topic) {
+        boolean result = kafkaTemplate.executeInTransaction((KafkaOperations<String, String> operations) -> {
+            operations.sendDefault("1");
+            operations.sendDefault("2");
+            return true;
+        });
+        log.info("topic:{} partitionInfos{}", topic, result);
+        return result;
 
-        return "success";
+    }
+
+    /**
+     * 获取partitioninfo
+     *
+     * @param topic
+     * @return
+     */
+    @RequestMapping("getPartitionInfos")
+    public String getPartitionInfos(@RequestParam String topic) {
+
+        List<PartitionInfo> partitionInfos = kafkaTemplate.partitionsFor(topic);
+        log.info("topic:{} partitionInfos{}", topic, partitionInfos);
+        return partitionInfos.toString();
 
     }
 
--- kafkatemplate/src/main/java/com/wotrd/kafkatemplate/utils/KafkaSender.java
+++ kafkatemplate/src/main/java/com/wotrd/kafkatemplate/utils/KafkaSender.java
@@ -1,7 +1,6 @@
 package com.wotrd.kafkatemplate.utils;
 
 import com.alibaba.fastjson.JSONObject;
-import com.wotrd.kafkatemplate.domain.Message;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
