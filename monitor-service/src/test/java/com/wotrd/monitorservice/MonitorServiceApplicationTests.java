76a09fdf24c3003c26b3965af1f622013dc70376
wangkj <wangkj@linkcld.com>
2020-06-26 12:54:38 +0800

    添加spring boot admin

---
+++ monitor-service/src/test/java/com/wotrd/monitorservice/MonitorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wotrd.monitorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
