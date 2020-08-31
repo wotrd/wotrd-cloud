59398a6e8e97cbaec2f7fa945b470479055753c7
wangkj <wangkj@linkcld.com>
2020-08-31 23:25:03 +0800

    认证服务：重新整理oauth2认证授权服务

--- wotrd-resource/src/main/java/com/wotrd/resourceserver/controller/ContentController.java
+++ wotrd-resource/src/main/java/com/wotrd/resourceserver/controller/ContentController.java
@@ -3,6 +3,7 @@ package com.wotrd.resourceserver.controller;
 import com.wotrd.resourceserver.domain.TbContent;
 import com.wotrd.resourceserver.service.ContentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ public class ContentController {
     @Autowired
     private ContentService contentService;
 
+    @PreAuthorize("hasAuthority('admin')")
     @RequestMapping("list")
     public List<TbContent> queryAllContent(@RequestParam("categoryId") Long categoryId) {
         return contentService.queryAllContent(categoryId);
