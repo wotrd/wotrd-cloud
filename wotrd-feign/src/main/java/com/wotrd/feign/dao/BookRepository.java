f48a42d581f8f7c2e79c81c0939d8b4a6efeb3ef
woniu <woniu@webuy.ai>
2021-05-13 20:06:24 +0800

    基于伯克利实现持久化队列

--- wotrd-feign/src/main/java/com/wotrd/feign/dao/BookRepository.java
+++ wotrd-feign/src/main/java/com/wotrd/feign/dao/BookRepository.java
@@ -1,12 +1,14 @@
-//package com.wotrd.gateway.dao;
-//
-//import com.wotrd.gateway.model.entity.BookDO;
-//import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface BookRepository extends ElasticsearchRepository<BookDO, Long> {
-//
-////    List<BookDO> findBookDOByAuthorNameAndAndPriceBetween(String name, Integer price);
-//
-//}
+package com.wotrd.feign.dao;
+
+import com.wotrd.feign.model.entity.BookDO;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BookRepository extends ElasticsearchRepository<BookDO, Long> {
+
+    List<BookDO> findBookDOByAuthorNameAndAndPriceBetween(String name, Integer price);
+
+}
